969f4662c2d9ddca08cb94091f7a4b3db750d6ac
carlumbo <carlpalumbo1@gmail.com>
2022-01-26 10:42:40 -0500

    updated app bar

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable prettier/prettier */
+/* eslint-disable jsx-quotes */
+/* eslint-disable quotes */
+import React from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { spacing } from './src/utils/sizes';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <View style={styles.searchContainer}>
+          <Searchbar />
+        </View>
+        <View style={styles.listContainer}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContianer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#559900',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchContainer: {
+    padding: spacing.sm,
+    backgroundColor: 'white',
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+  safeAreaContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
   },
 });
+
+//
---
+++ src/components/SearchBar.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable jsx-quotes */
+/* eslint-disable quotes */
+
+import * as React from 'react';
+import { Searchbar } from 'react-native-paper';
+
+const MyComponent = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <Searchbar
+      placeholder='Search'
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
+  );
+};
+
+export default MyComponent;
